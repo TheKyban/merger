17a552de20e7da4ad93ed0463244f0e1d2641bf1
Aditya <aaditya1392@gmail.com>
2023-03-03 20:16:47 +0530

    some fix

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const multer  = require('multer')
 
 const upload = multer({ dest: 'uploads/' })
 const app = express();
+
+const port = process.env.PORT || 7575
+
 app.use('/static', express.static('public'))
 
 
@@ -23,12 +26,12 @@ app.post('/merger', upload.array('pdfs', 2), async (req, res, next)=> {
     let p1 = path.join(__dirname, req.files[0].path);
     let p2 = path.join(__dirname, req.files[1].path)
     let d = await pdfMerger(p1, p2)
-    res.redirect(`http://localhost:7575/static/merge/${d}.pdf` )
+    res.redirect(`http://localhost:${port}/static/merge/${d}.pdf` )
     p1=""
     p2=""
   })
   
 
-app.listen(7575,()=>{
+app.listen(port,()=>{
     console.log("Server Started")
 })
